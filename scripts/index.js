e9014c0b27f0045d3ac812450023c609122d3aad
Andrey Goryachev <goryachev.andrej@yandex.ru>
2022-12-10 18:24:04 +0300

    fix: transfer array with cards to cards.js

--- scripts/index.js
+++ scripts/index.js
@@ -15,32 +15,6 @@ const popupCardInputName = document.querySelector('.popup__input_content_place')
 const popupCardInputLink = document.querySelector('.popup__input_content_link');
 const cards = document.querySelector('.elements__list');
 const formCardPopup = popupCard.querySelector('.popup__form');
-const initialCards = [
-  {
-    name: 'Водопад Шумка',
-    link: 'https://images.unsplash.com/photo-1634665609954-7aaf882505de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8JUQwJTkyJUQwJUJFJUQwJUI0JUQwJUJFJUQwJUJGJUQwJUIwJUQwJUI0JTIwJUQwJUE4JUQxJTgzJUQwJUJDJUQwJUJBJUQwJUIwfGVufDB8fDB8fA%3D%3D&w=1000&q=80'
-  },
-  {
-    name: 'Домбай',
-    link: 'https://images.unsplash.com/photo-1595849948760-cc6dcea6c162?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8JUQwJTk0JUQwJUJFJUQwJUJDJUQwJUIxJUQwJUIwJUQwJUI5fGVufDB8fDB8fA%3D%3D&w=1000&q=80'
-  },
-  {
-    name: 'Кондуки',
-    link: 'https://images.unsplash.com/photo-1619651797913-fa18e9ca7f31?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8JUQwJTlBJUQwJUJFJUQwJUJEJUQwJUI0JUQxJTgzJUQwJUJBJUQwJUI4fGVufDB8fDB8fA%3D%3D&w=1000&q=80'
-  },
-  {
-    name: 'Териберка',
-    link: 'https://images.unsplash.com/photo-1661517753182-40bc3b932479?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHRlcmliZXJrYXxlbnwwfHwwfHw%3D&w=1000&q=80'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://images.unsplash.com/photo-1617835594990-7cd5a9b5d153?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFpa2FsfGVufDB8fDB8fA%3D%3D&w=1000&q=80'
-  },
-  {
-    name: 'Телецкое озеро',
-    link: 'https://images.unsplash.com/photo-1596003903067-bf5762ad5c19?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fCVEMSU4MiVEMCVCNSVEMCVCQiVEMCVCNSVEMSU4NiVEMCVCQSVEMCVCRSVEMCVCNXxlbnwwfHwwfHw%3D&w=1000&q=80'
-  },
-];
 // Карточки с фото
 const listElements = document.querySelector('.elements__list');
 const popupImage = document.querySelector('.popup_content_photo');
@@ -141,7 +115,6 @@ function closePopupImageCard () {
   closePopup(popupImage);
   descriptionPopupImage.textContent = '';
 }
-
 
 // События
 // Загрузка страницы
