e2d8e07f28bcb52cc040bbf466b7252770db9417
Andrey Goryachev <goryachev.andrej@yandex.ru>
2023-02-05 22:13:33 +0300

    fix small bugs in the code

--- webpack.config.js
+++ webpack.config.js
@@ -19,26 +19,20 @@ module.exports = {
   },
   module: {
     rules: [ // rules — это массив правил
-      // добавим в него объект правил для бабеля
+      // добавим правило для бабеля
       {
-        // регулярное выражение, которое ищет все js файлы
         test: /\.js$/,
-        // при обработке этих файлов нужно использовать babel-loader
         use: 'babel-loader',
-        // исключает папку node_modules, файлы в ней обрабатывать не нужно
         exclude: '/node_modules/'
       },
       // добавили правило для обработки файлов
       {
-        // регулярное выражение, которое ищет все файлы с такими расширениями
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
         type: 'asset/resource'
       },
       {
-        // применять это правило только к CSS-файлам
+        // добавили правило для обработки CSS файлов
         test: /\.css$/,
-        // при обработке этих файлов нужно использовать
-        // MiniCssExtractPlugin.loader и css-loader
         use: [MiniCssExtractPlugin.loader, {
           loader: 'css-loader'
         }]
@@ -47,7 +41,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html' // путь к файлу index.html
+      template: './src/index.html'
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin()
