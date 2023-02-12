c7747785d9bc5eeae83f8c685c10d474304ae879
Andrey Goryachev <goryachev.andrej@yandex.ru>
2023-02-12 08:29:23 +0300

    feat: Deploy to gh-pages, add Api class

---
+++ src/components/Api.js
@@ -0,0 +1,37 @@
+export default class Api {
+  constructor(options) {
+    this._options = options
+  }
+
+  getProfile() {
+
+  }
+
+  getInitialCards() {
+    // ...
+  }
+
+  setProfile() {
+
+  }
+
+  addCard() {
+
+  }
+
+  getLikesCount() {
+
+  }
+
+  addLike() {
+
+  }
+
+  removeLike() {
+
+  }
+
+  updateAvatar() {
+
+  }
+}
