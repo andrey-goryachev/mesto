b45ecee775efed7af69af008a9de9b1f41947c04
Andrey Goryachev <goryachev.andrej@yandex.ru>
2023-02-23 10:51:26 +0300

    fix: sort constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,53 +1,36 @@
-// const initialCards = [
-//   {
-//     name: 'Водопад Шумка',
-//     link: 'https://images.unsplash.com/photo-1634665609954-7aaf882505de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8JUQwJTkyJUQwJUJFJUQwJUI0JUQwJUJFJUQwJUJGJUQwJUIwJUQwJUI0JTIwJUQwJUE4JUQxJTgzJUQwJUJDJUQwJUJBJUQwJUIwfGVufDB8fDB8fA%3D%3D&w=1000&q=80'
-//   },
-//   {
-//     name: 'Домбай',
-//     link: 'https://images.unsplash.com/photo-1595849948760-cc6dcea6c162?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8JUQwJTk0JUQwJUJFJUQwJUJDJUQwJUIxJUQwJUIwJUQwJUI5fGVufDB8fDB8fA%3D%3D&w=1000&q=80'
-//   },
-//   {
-//     name: 'Кондуки',
-//     link: 'https://images.unsplash.com/photo-1619651797913-fa18e9ca7f31?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8JUQwJTlBJUQwJUJFJUQwJUJEJUQwJUI0JUQxJTgzJUQwJUJBJUQwJUI4fGVufDB8fDB8fA%3D%3D&w=1000&q=80'
-//   },
-//   {
-//     name: 'Териберка',
-//     link: 'https://images.unsplash.com/photo-1661517753182-40bc3b932479?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHRlcmliZXJrYXxlbnwwfHwwfHw%3D&w=1000&q=80'
-//   },
-//   {
-//     name: 'Байкал',
-//     link: 'https://images.unsplash.com/photo-1617835594990-7cd5a9b5d153?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFpa2FsfGVufDB8fDB8fA%3D%3D&w=1000&q=80'
-//   },
-//   {
-//     name: 'Телецкое озеро',
-//     link: 'https://images.unsplash.com/photo-1596003903067-bf5762ad5c19?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fCVEMSU4MiVEMCVCNSVEMCVCQiVEMCVCNSVEMSU4NiVEMCVCQSVEMCVCRSVEMCVCNXxlbnwwfHwwfHw%3D&w=1000&q=80'
-//   },
-// ];
-
+// селекторы
 const selectorPopupProfile = '.popup_content_profile';
 const selectorPopupCard = '.popup_content_card';
-const popupProfile = document.querySelector(selectorPopupProfile);
-const buttonEditProfile = document.querySelector('.profile__button_function_edit');
-const profileName = document.querySelector('.profile__title');
-const profileDescription = document.querySelector('.profile__text');
-const popupProfileInputName = popupProfile.querySelector('.popup__input_content_name');
-const popupProfileInputDescription = popupProfile.querySelector('.popup__input_content_description');
-const formProfilePopup = popupProfile.querySelector('.popup__form');
-
-const popupCard = document.querySelector(selectorPopupCard);
-const buttonAddCard = document.querySelector('.profile__button_function_add');
-const formCardPopup = popupCard.querySelector('.popup__form');
-const selectorTemplateCard = '#elements__card';
-const selectorPopupContentPhoto = '.popup_content_photo';
-const selectorElementsList = '.elements__list';
-const avatar = document.querySelector('.profile__avatar')
 const selectorPopupWithConfirmation = '.popup_content_delete-card'
 const selectorElementBinActive = '.elements__bin_active'
 const selectorElementBin = '.elements__bin_active'
 const selectorPopupAvatar = '.popup_content_avatar'
-const formAvatarPopup = document.querySelector(selectorPopupAvatar).querySelector('.popup__form');
+const selectorTemplateCard = '#elements__card';
+const selectorPopupContentPhoto = '.popup_content_photo';
+const selectorElementsList = '.elements__list';
 
+// попапы
+const popupProfile = document.querySelector(selectorPopupProfile);
+const popupCard = document.querySelector(selectorPopupCard);
+const popupAvatar = document.querySelector(selectorPopupAvatar)
+
+// формы
+const formAvatarPopup = popupAvatar.querySelector('.popup__form');
+const formProfilePopup = popupProfile.querySelector('.popup__form');
+const formCardPopup = popupCard.querySelector('.popup__form');
+
+// элементы форм
+const popupProfileInputName = popupProfile.querySelector('.popup__input_content_name');
+const popupProfileInputDescription = popupProfile.querySelector('.popup__input_content_description');
+
+// профиль
+const profileName = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__text');
+const avatar = document.querySelector('.profile__avatar')
+const buttonEditProfile = document.querySelector('.profile__button_function_edit');
+const buttonAddCard = document.querySelector('.profile__button_function_add');
+
+// параметры валидации
 const settingsValidation = {
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
@@ -57,8 +40,8 @@ const settingsValidation = {
   errorClass: 'popup__error_visible'
 }
 
+// для апи
 const cohortId = 'cohort-60'
-
 const apiOptions = {
   token: '18e4741a-6c44-4105-91a3-2c8e1e3592b8',
   urlBase: `https://mesto.nomoreparties.co/v1/${cohortId}`
@@ -66,7 +49,6 @@ const apiOptions = {
 
 
 export {
-  // initialCards,
   buttonEditProfile,
   profileName,
   profileDescription,
