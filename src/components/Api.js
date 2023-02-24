0ca0b235b0b9902d5d5d7787d02693281fe6877c
Andrey Goryachev <goryachev.andrej@yandex.ru>
2023-02-24 17:08:24 +0300

    fix: correct the reviewer's comments in Api.js

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,79 +1,74 @@
 export default class Api {
   constructor(options) {
-    this._token = options.token
-    this._headers = {authorization: options.token}
-    this._urlBase = options.urlBase
+    this._token = options.token;
+    this._headers = { authorization: options.token };
+    this._urlBase = options.urlBase;
 
-    this._urlGetProfile = `${this._urlBase}/users/me`
-    this._urlGetCards = `${this._urlBase}/cards`
-    this._urlSetProfile = `${this._urlBase}/users/me`
-    this._urlAddCard = `${this._urlBase}/cards`
-    this._urlUpdateAvatar = `${this._urlBase}/users/me/avatar`
+    this._urlProfile = `${this._urlBase}/users/me`;
+    this._urlGetCards = `${this._urlBase}/cards`;
+    this._urlAddCard = `${this._urlBase}/cards`;
+    this._urlUpdateAvatar = `${this._urlBase}/users/me/avatar`;
   }
 
-  _requestServer(url, method, dataObject) {
-    let options
+  _requestServer(url, method='GET', dataObject) {
+    let options;
     if (method === 'GET') {
       options = {
         method: method,
-          headers: {authorization: this._token}
-      }
+        headers: { authorization: this._token },
+      };
     } else {
       options = {
         method: method,
         headers: {
           authorization: this._token,
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify(dataObject)
-      }
+        body: JSON.stringify(dataObject),
+      };
     }
 
-    return fetch(url, options)
-      .then((res) => {
-        if (res.ok) {
-          return res.json()
-        } else {
-          return Promise.reject(`Ошибка!!! статус ${res.status}`)
-        }
-      })
-      .then((res) => {
-        return res
-      })
+    return fetch(url, options).then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        return Promise.reject(`Ошибка!!! статус ${res.status}`);
+      }
+    });
   }
 
   getProfile() {
-    return this._requestServer(this._urlGetProfile, 'GET')
+    return this._requestServer(this._urlProfile);
   }
 
   getInitialCards() {
-    return this._requestServer(this._urlGetCards, 'GET')
+    return this._requestServer(this._urlGetCards);
   }
 
   setProfile(userObject) {
-    return this._requestServer(this._urlSetProfile, 'PATCH', userObject)
+    return this._requestServer(this._urlProfile, 'PATCH', userObject);
   }
 
   addCard(cardObject) {
-    return this._requestServer(this._urlAddCard, 'POST', cardObject)
+    return this._requestServer(this._urlAddCard, 'POST', cardObject);
   }
 
   deleteCard(deleteElement) {
-    this._urlDeleteCard = `${this._urlBase}/cards/${deleteElement.id}`
-    return this._requestServer(this._urlDeleteCard, 'DELETE')
+    this._urlDeleteCard = `${this._urlBase}/cards/${deleteElement.id}`;
+    return this._requestServer(this._urlDeleteCard, 'DELETE');
   }
 
   addLike(cardId) {
-    this._urlLike = `${this._urlBase}/cards/${cardId}/likes`
-    return this._requestServer(this._urlLike, 'PUT')
+    this._urlLike = `${this._urlBase}/cards/${cardId}/likes`;
+    return this._requestServer(this._urlLike, 'PUT');
   }
 
   removeLike(cardId) {
-    this._urlLike = `${this._urlBase}/cards/${cardId}/likes`
-    return this._requestServer(this._urlLike, 'DELETE')
+    this._urlLike = `${this._urlBase}/cards/${cardId}/likes`;
+    return this._requestServer(this._urlLike, 'DELETE');
   }
 
   updateAvatar(avatarLink) {
-    return this._requestServer(this._urlUpdateAvatar, 'PATCH', {avatar: avatarLink})
+    return this._requestServer(this._urlUpdateAvatar, 'PATCH', { avatar: avatarLink });
   }
 }
